739dde2fd0f92dc48142cad801f468da87cb3fcc
Slava Protashchik <v.protashchik@syberry.com>
2015-07-19 14:57:06 +0300

    Fixed produnct content in list

--- www/wp-content/themes/lapata/woocommerce/content-product.php
+++ www/wp-content/themes/lapata/woocommerce/content-product.php
@@ -41,139 +41,90 @@ if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 
 if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 	$classes[] = 'last'; */
 ?>
-<!-- <li <?php post_class( $classes ); ?>> -->
 <figure>
-<div class="product-block">
-	<!--<?php //do_action( 'woocommerce_before_shop_loop_item' ); ?> -->
-		<div class="imagecontainer">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-		    <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'archive-page-m'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="150px" height="100px" />'; ?>
-			</a>
-		</div>
-		
-		<div class="descrcontainer">
-			<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-			<?php $alt=1; ?>
-			<div class="shortparams">
-				<?php 
-				$attributes = $product->get_attributes();
-					if ($attributes ) {
-						$loop 		= 0;
-						foreach ( $attributes as $attribute ) {
-							if ($loop>=5) {break 1;}
-							if ( mb_substr($attribute['name'],-2)=="__") { 
+    <div class="product-block">
+        <!--<?php //do_action( 'woocommerce_before_shop_loop_item' ); ?> -->
+            <div class="imagecontainer">
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'archive-page-m'); else echo '<img src="'.wc_placeholder_img_src().'" alt="Placeholder" width="150px" height="100px" />'; ?>
+                </a>
+            </div>
+            <div class="descrcontainer">
+                <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+                <?php $alt=1; ?>
+                <div class="shortparams">
+                    <?php
+                    $attributes = $product->get_attributes();
+                    $attributes = array_filter($attributes, function($attr){
+                        return (mb_substr($attr['name'],-2)=="__");
+                    });
+                    array_slice($attributes, 5);
+                    ?>
+                    <?php if ($attributes ) : ?>
+                        <?php foreach ( $attributes as $attribute ) : ?>
+                            <div class="shortparams-row <?php if ( ( $alt = $alt * -1 ) <> 1 ) echo 'alt'; ?>">
+                                <div class="shortparams-cell">
+                                    <?php echo wc_attribute_label( $attribute['name'] ) . ': '; ?>
+                                </div>
+                                <div class="shortparams-cell">
+                                    <?php if ( $attribute['is_taxonomy'] ) : ?>
+                                        <?php $values = wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) ); ?>
+                                        <?php echo  apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ), $attribute, $values ); ?>
+                                    <?php  else : ?>
+                                        <?php $values = array_map( 'trim', explode( WC_DELIMITER, $attribute['value'] ) ); ?>
+                                        <?php echo apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ) , $attribute, $values ); ?>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        <?php endforeach ?>
+                    <?php endif; ?>
+                </div>
+            </div>
 
-				?>
+            <div class="pricecontainer">
+                <p class="price"><?php echo $product->get_price_html(); ?></p>
 
-							<div class="shortparams-row <?php if ( ( $alt = $alt * -1 ) <> 1 ) echo 'alt'; ?>">
-								<div class="shortparams-cell">
-									<?php echo wc_attribute_label( $attribute['name'] ) . ': '; ?>
-								</div>
-								<div class="shortparams-cell">
-									<?php 
-									if ( $attribute['is_taxonomy'] ) {
-														$values = wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) );
-														echo  apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ), $attribute, $values );
+                <div class="starscontainer">
+                <?php
+                    if ($product->get_rating_html() =="") {echo "<p>без рейтинга</p>";}
+                    else {echo $product->get_rating_html();}
+                ?>
 
-													} else {
-														// Convert pipes to commas and display values
-														$values = array_map( 'trim', explode( WC_DELIMITER, $attribute['value'] ) );
-														echo apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ) , $attribute, $values );
-													}
-									?>
-								</div>					
-							</div>
+                </div>
 
-							<?php 
-							$loop++;
-							} 
-							
-						}
-					}?>
-			</div>
-<?php
-/*
-$before_out='<p class="out">';
-$after_out='</p>';
-$attributes = $product->get_attributes();
-if ($attributes ) {
-    
-    
-$out = '';
-    foreach ( $attributes as $attribute ) {
-    	
-    	if ( mb_substr($attribute['name'],3,1)=="_") {
-        $out .= wc_attribute_label( $attribute['name'] ) . ': ';
-        	
-        	if ( $attribute['is_taxonomy'] ) {
-					$values = wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) );
-					$out .=  apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ), $attribute, $values );
+                <div class="button-container">
+                <?php
+                $disabled="";
+                if (!$product->is_in_stock()) {$disabled="disabled";}
+                echo apply_filters( 'woocommerce_loop_add_to_cart_link',
+                    sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button %s product_type_%s %s">%s</a>',
+                        esc_url( $product->add_to_cart_url() ),
+                        esc_attr( $product->id ),
+                        esc_attr( $product->get_sku() ),
+                        esc_attr( isset( $quantity ) ? $quantity : 1 ),
+                        $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+                        esc_attr( $product->product_type ),
+                        esc_attr( $disabled ),
+                        esc_html( 'В корзину' )
+                    ),
+                $product );
+                ?>
+                </div>
 
-				} else {
-					// Convert pipes to commas and display values
-					$values = array_map( 'trim', explode( WC_DELIMITER, $attribute['value'] ) );
-					$out .=  apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ) , $attribute, $values );
-				}
+                <?php if ( $product->is_in_stock() ) {
+                    //наличие товара на складе -->
+                    $class="in-stock";
+                    $mes="<span>в наличии</span>";
+                } else {
+                    $class="out-of-stock";
+                    $mes="<span>отсутствует</span>";
+                }
 
-		$out .=', ';
-		}
-    }
-
-
-//echo textFunc( $out, 50 ); 
-$out_final=textFunc( $out, 200, '<a href="'.get_the_permalink().'" title="'.get_the_title().'">подробнее...</a>');
-echo $before_out.$out_final.$after_out;	
-}
-*/?>
-
-
-		</div>
-		
-		<div class="pricecontainer">
-			<p class="price"><?php echo $product->get_price_html(); ?></p>
-
-			<div class="starscontainer">
-			<?php 
-				if ($product->get_rating_html() =="") {echo "<p>без рейтинга</p>";} 
-				else {echo $product->get_rating_html();}
-			?>
-
-	        </div>
-			
-			<div class="button-container">
-			<?php
-			$disabled="";
-			if (!$product->is_in_stock()) {$disabled="disabled";}
-			echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-				sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button %s product_type_%s %s">%s</a>',
-					esc_url( $product->add_to_cart_url() ),
-					esc_attr( $product->id ),
-					esc_attr( $product->get_sku() ),
-					esc_attr( isset( $quantity ) ? $quantity : 1 ),
-					$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
-					esc_attr( $product->product_type ),
-					esc_attr( $disabled ),
-					esc_html( 'В корзину' )
-				),
-			$product );
-			?>
-			</div>
-
-			<?php if ( $product->is_in_stock() ) {
-				//наличие товара на складе -->
-				$class="in-stock";
-				$mes="<span>в наличии</span>";
-			} else {
-				$class="out-of-stock";
-				$mes="<span>отсутствует</span>";								
-			}
-
-			echo '<div class="'.$class.'" >';
-			include(dirname(__FILE__)."/../images/".$class.".svg"); //вставляем svg картинку
-			echo $mes.'</div>';
-			//конец наличия товара на складе-->
-			?>
-		</div>	
-
-</div>
+                echo '<div class="'.$class.'" >';
+                include(dirname(__FILE__)."/../images/".$class.".svg"); //вставляем svg картинку
+                echo $mes.'</div>';
+                //конец наличия товара на складе-->
+                ?>
+            </div>
+    </div>
 </figure>
